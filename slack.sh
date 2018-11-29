95883e6fdb544fba67491930a079c453b0162204
Luis Miglietti <luismiglietti@gmail.com>
2018-11-29 15:24:10 +0100

    added colors, removed emoji, json format

--- slack.sh
+++ slack.sh
@@ -14,22 +14,26 @@ username='Zabbix'
 # Get the Slack channel or user ($1) and Zabbix subject ($2 - hopefully either PROBLEM or RECOVERY/OK)
 to="$1"
 subject="$2"
+subject_status=`echo $subject | cut -d ' ' -f1`
+severity=`echo $subject | cut -d ' ' -f3`
 
-# Change message emoji depending on the subject - smile (RECOVERY/OK), frowning (PROBLEM), or ghost (for everything else)
 recoversub='^RECOVER(Y|ED)?$'
-if [[ "$subject" =~ ${recoversub} ]]; then
-	emoji=':smile:'
-elif [ "$subject" == 'OK' ]; then
-	emoji=':smile:'
-elif [ "$subject" == 'PROBLEM' ]; then
-	emoji=':frowning:'
-else
-	emoji=':ghost:'
+if [[ "$subject_status" =~ ${recoversub} ]]; then
+	color='#20E020' # green text
+elif [ "$subject_status" == 'PROBLEM' ]; then
+	color='FFF333'    # yellow text - warning
+	if [[ "$severity" == 'Average' ]]; then
+		color='FFA233' # orange text
+	elif [[ "$severity" == 'High' ]]; then
+		color='FF3333' # red text
+	elif [[ "$severity" == 'Disaster' ]]; then
+                color='D80000' # dark red text
+	fi
 fi
 
 # The message that we want to send to Slack is the "subject" value ($2 / $subject - that we got earlier)
 #  followed by the message that Zabbix actually sent us ($3)
-message="${subject}: $3"
+message="$3"
 
 # in case a 4th parameter is set, we will use it for the url
 url=${4-$url}
@@ -40,5 +44,17 @@ if [[ "$proxy" != "" ]] ; then
 fi
 
 # Build our JSON payload and send it as a POST request to the Slack incoming web-hook URL
-payload="payload={\"channel\": \"${to//\"/\\\"}\", \"username\": \"${username//\"/\\\"}\", \"text\": \"${message//\"/\\\"}\", \"icon_emoji\": \"${emoji}\"}"
-curl $proxy -m 5 --data-urlencode "${payload}" $url -A 'zabbix-slack-alertscript / https://github.com/ericoc/zabbix-slack-alertscript'
+payload="payload={
+        \"channel\": \"${to}\",
+	\"username\": \"${username}\",
+        \"icon_emoji\": \":zabbix:\",
+	\"attachments\": [
+           {
+		\"title\": \"${subject}\",
+                \"text\": \"${message}\",
+	        \"fallback\": \"${message}\",
+                \"color\": \"${color}\",
+                \"mrkdwn_in\": [ \"text\" ]
+           }
+        ] }"
+/usr/bin/curl $proxy -m 5 --data-urlencode "${payload}" $url -A 'zabbix-slack-alertscript / https://github.com/ericoc/zabbix-slack-alertscript'
